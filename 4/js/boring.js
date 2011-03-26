c3b530a866c32be46f3d3c039def42de9e8423e1
Adam Lloyd <adam@alloy-d.net>
2011-03-26 16:55:17 -0400

    Add polar coordinate conversion.

--- 4/js/boring.js
+++ 4/js/boring.js
@@ -13,3 +13,19 @@ var wr = function (n) { return Math.floor(Math.random() * n) }
 
 // random noise with absolute value < n
 var rn = function (n) { return n - wr(2*n) }
+
+// Map polar coordinates to rectangular coordinates,
+// optionally centered at (cx, cy).
+var rect = function (r, theta, cx, cy) {
+    if (typeof(cx) !== "number" || typeof(cy) !== "number") {
+        cx = 0;
+        cy = 0;
+    }
+
+    theta = (theta * Math.PI / 360);
+
+    return {
+        x: Math.cos(theta) * r + cx,
+        y: Math.sin(theta) * r + cy,
+    }
+}
